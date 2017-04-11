ed5866041d767836c5d2bf46dbac17bbc447313d
CHANG JIAN-YI <CHANG JIAN-YI>
2017-04-11 22:15:16 +0800

    add example2

---
+++ Lecture01/Example02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example02
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            student student = new student();
+
+            student.Name = "張建益";
+            student.Email = "jianyi.chang@gmail.com";
+        }
+    }
+}
