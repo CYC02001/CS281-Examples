bf41bc514dc27bc0ae521855b9972c82cd8bd7e8
CHANG JIAN-YI <CHANG JIAN-YI>
2017-04-16 19:57:28 +0800

    add Lecture02

---
+++ lLecture02/Example03-bool/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example03_bool
+{
+    public class BoolKeyTest
+    {
+        private static void Main()
+        {
+            Console.Write("Enter a character: ");
+            char c = (char)Console.Read();
+            if (Char.IsLetter(c))
+            {
+                if (Char.IsLower(c))
+                {
+                    Console.WriteLine("The character is lowercase.");
+                }
+                else
+                {
+                    Console.WriteLine("The character is uppercase.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not an alphabetic character.");
+            }
+        }
+    }
+}
+
+/* Sample Output:
+
+    Enter a character: X
+    The character is uppercase.
+
+    Enter a character: x
+    The character is lowercase.
+
+    Enter a character: 2
+    The character is not an alphabetic character.
+ */
